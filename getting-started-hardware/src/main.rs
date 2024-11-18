4147080086a733bf51438e430e7d0ee5c1a86e96
ziedmasmoudi <zied.masmoudi2001@gmail.com>
2024-11-18 00:21:21 -0500

    cleanup

--- getting-started-hardware/src/main.rs
+++ getting-started-hardware/src/main.rs
@@ -1,20 +1,15 @@
-#![no_std]
+//! Prints "Hello, world!" on the host console using semihosting
+
 #![no_main]
+#![no_std]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _;
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
-    loop {
-        // your code goes here
-    }
+    hprintln!("Hello, world!").unwrap();
+    loop {}
 }
